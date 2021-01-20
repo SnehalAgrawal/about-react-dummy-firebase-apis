c6d68195fa6832d667b9c434eb797cb7c8b401db
SnehalAgrawal <snehalagrawal1701@gmail.com>
2021-01-20 08:58:09 +0530

    First Release

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# about-react-dummy-firebase-apis
-Node API for Firebase
+# To Run the Project
+
+- Clone Repo
+
+  ```bash
+  git clone https://github.com/SnehalAgrawal/about-react-dummy-firebase-apis.git
+  ```
+
+- Jump into the directory
+
+  ```bash
+  cd about-react-dummy-firebase-apis
+  ```
+
+- Install:
+
+  ```bash
+   npm install
+  ```
+
+- Run the app:
+  ```bash
+   npm start
+  ```
+
+# APIs and Payload
+
+1. **Notify**\
+   URL: http://localhost:3000/notify \
+   Method: GET
+
+## By
+
+[AboutReact](https://aboutrect.com)
---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const admin = require("firebase-admin");
+const app = express();
+const port = 3000;
+const serviceAccount = require("/Users/kepi/Desktop/MainRepo/service-account-file.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/notify", async (req, res) => {
+  const message = {
+    topic: "MyNews",
+    notification: {
+      title: "Test Notification",
+      body: "Here is the Notification Description",
+    },
+    data: {
+      name: "AboutReact",
+      url: "https://aboutreact.com",
+      writter: "Snehal Agrawal",
+    },
+    android: {
+      notification: {
+        image:
+          "https://raw.githubusercontent.com/AboutReact/sampleresource/master/old_logo.png",
+      },
+      ttl: 4500,
+      priority: "normal",
+    },
+    apns: {
+      headers: {
+        "apns-priority": "5",
+        "apns-expiration": "1604750400",
+      },
+    },
+  };
+
+  admin
+    .messaging()
+    .send(message)
+    .then((response) => {
+      res.send(`Successfully sent message: ${response}`);
+    })
+    .catch((error) => {
+      res.send(`Error sending message: ${error}`);
+    });
+});
+
+app.listen(port, () => {
+  console.log(
+    `Example app listening at http://localhost:${port}`
+  );
+});
